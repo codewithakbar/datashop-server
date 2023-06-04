73e7ac90b29fd943a250720b2b76ad9414cb1a3c
Satipov Akbar <88106359+codewithakbar@users.noreply.github.com>
2023-06-04 18:29:16 +0500

    urls add static conf

--- backend/urls.py
+++ backend/urls.py
@@ -1,9 +1,20 @@
 
 from django.contrib import admin
 from django.urls import path
-from django.urls import path, include
+from django.urls import path, include, re_path
+
+from django.conf.urls.static import static
+from django.conf import settings
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include("api.urls"))
 ]
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT, }), ]
+urlpatterns += [re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), ]
+
+
