c7fa0fb9abb883871aed4214b29488d3cd5bede1
Satipov Akbar <88106359+codewithakbar@users.noreply.github.com>
2023-06-04 17:54:21 +0500

    static root add

--- backend/settings.py
+++ backend/settings.py
@@ -160,11 +160,15 @@ USE_I18N = True
 
 USE_TZ = True
 
-
+import os
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
